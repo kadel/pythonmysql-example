aa601adfd103e2e244269649a9d2438cc8d1e545
Tomas Kral <tkral@redhat.com>
2021-02-12 17:05:22 +0100

    flush output

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import MySQLdb
 import os
+import sys
 import time
 
+import MySQLdb
 
 print("environment dump:")
-print("_______________-")
+print("_______________")
 for k,v in os.environ.items():
     print(k, "=", v)
-print("_______________-")
+print("_______________\n")
 
 username = os.environ.get('DB_USER')
 password = os.environ.get('DB_PASSWORD')
@@ -20,10 +21,14 @@ print("password", password)
 print("database_name", database_name)
 print("host", host)
 
+if host == None:
+    print("No database configured")
+    sys.exit(1)
+
 db = MySQLdb.connect(host, username, password, database_name)
 
 print(db)
 
 while True:
-    print(db.ping())
+    print(db.ping(), flush=True)
     time.sleep(10)
